14e3ebb967caf1262cf06dfac24cbba914da0557
Kristian Bauer <kristianbauer@twitter.com>
2016-01-29 22:12:50 -0500

    Basic game setup.

---
+++ Miskatonic Elementary/Assets/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float moveFactor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 moveVector = new Vector3 (Input.GetAxisRaw ("Horizontal") * moveFactor, Input.GetAxisRaw ("Vertical") * moveFactor, 0);
+		this.transform.position += moveVector;
+	}
+}
